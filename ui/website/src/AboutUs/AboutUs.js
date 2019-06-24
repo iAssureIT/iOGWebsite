804ac7f66b7e1a74ad1da0632cc6ad3c15db522b
ManaliGujarathi <mann272@gmail.com>
2019-06-24 20:07:22 +0530

    Manali added changes in About us page

--- ui/website/src/AboutUs/AboutUs.js
+++ ui/website/src/AboutUs/AboutUs.js
@@ -75,17 +75,16 @@ export default class AboutUs extends Component {
                         <div className="col-lg-12">
                               <div className="col-lg-6">
                                   <div className="col-lg-12">
-                                      <h2 className="textcolorblue para-top">Company Profile</h2>
+                                      <h2 className="textcolorblue para-top"><b>Company</b> Profile</h2>
                                       <p className="text-justify ">
-                                       iOG Solutions stands for intelligent O&G solutions.
-                                       iOG Solutions is an independent and reputed provider of consulting and 
-                                       implementation services for advanced and intelligent Software solutions 
-                                       in the Oil & Gas industry. Our headquarters have been established since 
-                                       2013 in Pune (India).
+                                       <div className="line_subhead">iOG Solutions stands for<br></br>
+                                       intelligent O&G solutions.</div>
+                                       <br></br>
+                                       <p className="line_para">iOG Solutions is an independent and reputed <b>provider of con-<br></br>
+                                       sulting and implementation services on advanced and intel-<br></br>
+                                       ligent Software solutions</b>in the Oil & Gas industry. Our head-<br></br>
+                                       quarters have been established since 2013 in Pune (India).
                                        </p>
-
-                                       <p className="text-justify ">
-                                       We always work with a strong motive to provide our clients with best-in-class services of international standards. We are an incredible team of professionals with rich experience in various domains across the Oil & Gas industry. We constantly strive hard to gain insights in our customer’s business module and provide innovative solutions that integrate discretely operating business processes across the value chain to a unified profit making center with clear alignment to the goals and objectives of our client.
                                        </p>
                                   </div>
                               </div>
@@ -99,23 +98,16 @@ export default class AboutUs extends Component {
                               <div className="col-lg-6">
                                   <div className="col-lg-12 ">
                                       <h2></h2>
-                                        <p className="text-justify pull-right">
-                                         As we serve the Oil & Gas industry across the world, we constantly 
-                                         encounter challenges and we work smart and hard to turn them into a rich experience.
-                                         </p>
-
-                                       <p className="text-justify pull-right">
-                                         iOG has a strong domain experience in Oil & Gas industry and
-                                         experience on leading software solutions in the area of MES,
-                                         SCM, Modeling, Optimization and other software solutions.
-                                         We deliver true results, focus on decisions and practical actions
-                                         tailored for our clients’ unique requirement.
-                                       </p>
-
-                                       <p className="text-justify pull-right">
-                                         We engage at all levels of an organization, build momentum and capabilities to sustain success. 
-                                         Our key strength is personnel by creating an enriching atmosphere that allows us to attract,
-                                         train and retain the best talent.
+                                        <p className="line_para text-justify pull-right">
+                                        We have a strong team of team of motivated<br></br>
+                                        and self-driven consultants with strong<br></br>
+                                        domain knowledge in areas of Supply Chain<br></br>
+                                        Management (SCM), Manufacturing Execu-<br></br>
+                                        tion Systems (MES), Simulation & Modeling,<br></br>
+                                        Energy Management, Asset Reliability & In-<br></br>
+                                        tegrity Management (RIM), Environmen-<br></br>
+                                        tal-Health-Safety (EHS), Enabling Technolo-<br></br>
+                                        gies and Oil & Gas E&P services.<br></br>
                                          </p>
                                   </div>
                               </div>
@@ -127,19 +119,19 @@ export default class AboutUs extends Component {
                         <div className="col-lg-12">
                                   <div className="col-lg-12 ">
                                       <h2 className="textcolorblue text-center para1-top"><b>Our</b> Engagement Model</h2>
-                                      <p className="text-center ">
-                                          iOG delivers services to the industry through 4 distinct arms of engagement.
-                                          We would consider it our privilege to join hands and work with you to explore 
-                                          every possibility of enhancing your company’s profitability. We are only a phone
-                                          call or email away from you.Looking forward to an opportunity to partner with you.
+                                      <br></br>
+                                      <p className="line_para  text-center">
+                                          iOG delivers services to the industry through 4 distinct arms of engagement.We would consider it our privi-<br></br>
+                                          lege to join hands and work with you to explore every possibility of enhancing your company’s profitability.<br></br>
+                                           We are only a phone call or email away from you.Looking forward to an opportunity to partner with you.<br></br>
                                         </p>          
                                   </div>
                               </div>
                               <div className="col-lg-3 block block1height">
                               <p className="iog_txt"><b>iOG</b>Study</p>
                               </div>
-                              <div className="col-lg-3 block1 block2height">
-                              <p className="iog_txt1"><b>iOG</b>Implement</p>
+                              <div className="col-lg-3 block block1height">
+                              <p className="iog_txt"><b>iOG</b>Implement</p>
                               </div>
                               <div className="col-lg-3 block block1height">
                               <p className="iog_txt"><b>iOG</b>Consult</p>
@@ -155,22 +147,14 @@ export default class AboutUs extends Component {
                               <div className="btm_txt col-lg-6">
                                   <div className="col-lg-12 ">
                                       <h2></h2>
-                                      <p className="text-justify">
-                                      iOG operates through a team of young and innovative consultants which has strong domain
-                                      knowledge in upstream and downstream aspects of the O & G industry and all leading software
-                                      solutions pertaining to the industry.
-                                       </p>
-
-                                       <p className="text-justify">
-                                          The team has executed many projects within the last few years spanning Europe,
-                                          the Middle East, India, and South East Asia..
-                                       </p>
-
-                                       <p className="text-justify">
-                                      Needless to add, the impact of iOG interventions and its support is directly reflected 
-                                      in the phenomenal growth and progress of the company engaging its services.
-
-                                       </p>
+                                      <p className="line_para text-justify">
+                                      Our key strength is our Team. iOG operates through a team
+                                      of young and innovative consultants which has strong
+                                      domain knowledge in all aspects of the O&G value chain
+                                      and expertise in all leading software solutions. Needless to
+                                      add, the impact of iOG interventions and its support is directly reflected in the phenomenal growth and progress of
+                                      the company engaging its services.
+                                      </p>
                                   </div>
                               </div>
                               <div className="col-lg-6"></div>
@@ -182,19 +166,19 @@ export default class AboutUs extends Component {
                          <div className="col-lg-6"></div>
                               <div className=" col-lg-6">
                                   <div className="col-lg-12 block block1height b1width">
-                                      <h2 className="textcolorblue vision_align1">VISION</h2>
+                                      <h2 className="line_subhead textcolorblue vision_align1">VISION</h2>
                                       <p className="text-justify vision_align">
-                                      To be a global market leader in Intelligent Support
-                                      Systems for Oil and Gas Industry.
+                                      <b>To be a global market leader in Intelligent Support
+                                      Systems for Oil and Gas Industry.</b>
                                        </p>
                                        </div>
                                        <div className="col-lg-12 block block1height b2width">
-                                       <h2 className="textcolorblue mission_align">MISSION</h2>
+                                       <h2 className="line_subhead textcolorblue mission_align">MISSION</h2>
                                        <p className="text-justify">
-                                          Nurture a talented team of professionals to provide
+                                          <b>Nurture a talented team of professionals to provide
                                           best-in-class consulting and implementation servic-
                                           es improving the profitability of our valued clients
-                                          across the globe.
+                                          across the globe.</b>
                                        </p>
                                        </div>                                       
                                   </div>
@@ -206,32 +190,27 @@ export default class AboutUs extends Component {
                         <div className="col-lg-6"></div>
                               <div className="col-lg-6">
                                   <div className="col-lg-12 ">
-                                      <h2 className="txt-size-director textcolorblue"><b>Pankaj Zawar</b></h2>
+                                      <h2 className="para-top3 txt-size-director textcoloryellow"><b>Pankaj Zawar</b></h2>
                                       <h1 className="txt-size-founder">Founder and Director of iOG Solutions</h1>
                                         <p className="para_spacing text-justify pull-right">
-                                        Our Founder and Director, Pankaj Zawar is a chemical engineer from
+                                        Our Founder and Director,<b> Pankaj Zawar</b> is a chemical engineer from
                                         UICT, Mumbai with 20+ years of experience in providing consulting, en-
-                                        gineering and implementation services for MES, APC and SCM solu-
-                                        tions. Pankaj has worked with Reliance at Jamnagar refinery as PIMS &
-                                        Optimization consultant. He then moved to business consulting and ex-
-                                        panded the scope to MES, APC, and SCM at Aspen Technology. He has
-                                        been at the forefront of consulting with several remarkable projects at
-                                        various national and international oil and gas majors. He has been the
-                                        distinguished recipient of prestigious President Award for Excellence
-                                        twice at Aspen Technology
-                                         </p>                                      
+                                        gineering and implementation services for MES, APC and SCM solutions.
+                                         </p> 
+                                        
+                                       <ul>
+                                      <li className="alg_lft">Strong domain knowledge in Oil and Gas industry</li>
+                                      <li className="alg_lft">Strong skills in using refinery LP and Scheduling tools 
+                                          for refinery production planning and scheduling.</li>
+                                      <li className="alg_lft">Led several remarkable MES and SCM projects at state-owned.<br></br>
+                                          Private refineries and petrochemical plants in India, SE Asia, and the Middle East.</li>
+                                      </ul>                                     
                                   </div>
                               </div>
                               
                             </div>
                         </div>
-                      </div> 
-
-                       
-
-
-                 
-                   
+                      </div>        
 
         );
     } 
