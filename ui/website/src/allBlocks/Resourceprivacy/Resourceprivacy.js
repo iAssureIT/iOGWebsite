546d46a2d545ae7b3f543b2808c556fc03d3a44e
ManaliGujarathi <mann272@gmail.com>
2019-07-17 20:06:58 +0530

    Manali modified into Resources Page

--- ui/website/src/allBlocks/Resourceprivacy/Resourceprivacy.js
+++ ui/website/src/allBlocks/Resourceprivacy/Resourceprivacy.js
@@ -22,42 +22,18 @@ export default class Resourceprivacy extends Component {
                       </p>          
                 </div>
                 <div  className="col-lg-12 blk_ht">
-                        <div className="col-lg-3  col-md-3 col-sm-12 col-xs-12">
+                        <div className="col-lg-12  col-md-12 col-sm-12 col-xs-12 col-lg-offset-4 center_block">
                           <div className="aboutus_block aboutus_block1height">
                           <div className="icon1">
                           <img src="/images/Icon_1.png" alt="" className="icn_1" />
                           <img src="/images/11.png" alt="" className="icn1_hover"  />
                           </div>
-                            <p className="iog_txt"><b>iOG</b>Study</p>
+                            <p className="iog_txt"><b>iOG</b>Policy</p>
                           </div>
                         </div>
-                        <div className="col-lg-3 col-md-3 col-sm-12 col-xs-12">
-                          <div className="aboutus_block aboutus_block1height">
-                          <div className="icon2">
-                          <img src="/images/Icon_2.png" alt="" className="icn_2" />
-                          <img src="/images/12.png" alt="" className="icn2_hover"  />
-                          </div>
-                          <p className="iog_txt "><b>iOG</b>Implement</p>
-                          </div>
-                        </div>
-                        <div className="col-lg-3 col-md-3 col-sm-12 col-xs-12">
-                          <div className="aboutus_block aboutus_block1height">
-                          <div className="icon3">
-                          <img src="/images/Icon_3.png" alt="" className="icn_3" />
-                          <img src="/images/13.png" alt="" className="icn3_hover"  />
-                          </div>
-                          <p className="iog_txt "><b>iOG</b>Consult</p>
-                          </div>
-                        </div>
-                        <div className="col-lg-3 col-md-3 col-sm-12 col-xs-12">
-                          <div className="aboutus_block aboutus_block1height">
-                          <div className="icon4">
-                          <img src="/images/Icon_4.png" alt="" className="icn_3" />
-                          <img src="/images/14.png" alt="" className="icn4_hover"  />
-                          </div>
-                          <p className="iog_txt "><b>iOG</b>Train</p>
-                          </div>
-                        </div>
+                        
+                       
+                        
                  </div>
             </div>
 
