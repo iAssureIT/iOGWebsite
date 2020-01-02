848f0bd858390c49b9b24c27b4897e926d6d00c3
karuna <amitrshinde156@gmail.com>
2020-01-02 15:03:20 +0530

    karuna added changes in resourcesvideos

--- ui/website/src/allBlocks/Resourcevideo/Resourcevideo.js
+++ ui/website/src/allBlocks/Resourcevideo/Resourcevideo.js
@@ -20,19 +20,19 @@ export default class Resourcevideo extends Component {
                 downstreamimg   : "/images/Illustration_2.png",
                 video1st        : "https://iogsolutions-my.sharepoint.com/personal/anurag_shinde_iogsolutions_com/_layouts/15/onedrive.aspx?originalPath=aHR0cHM6Ly9pb2dzb2x1dGlvbnMtbXkuc2hhcmVwb2ludC5jb20vOmY6L3AvYW51cmFnX3NoaW5kZS9Fc0VrNEo3cWtVaEx1UlRNNjBEUUttTUJndDB1WElqTjUwRmNjLU5ZT3dIUEp3P3J0aW1lPWFfRjg1Wlp0MTBn&id=%2Fpersonal%2Fanurag%5Fshinde%5Fiogsolutions%5Fcom%2FDocuments%2FMarketing%20Activities%2FWebsite%20Revamp%2FVideos%2F05%20iOG%20SCM%2Emp4&parent=%2Fpersonal%2Fanurag%5Fshinde%5Fiogsolutions%5Fcom%2FDocuments%2FMarketing%20Activities%2FWebsite%20Revamp%2FVideos",
                 downstreamtext  : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pellentesque, elit id pharetra cursus, turpis ex mollis magna, eu fringilla urna ante ut tellus. Cras imperdiet tristique venenatis Vivamus elementum enim ipsum, nec pharetra sapien ornare eu. Mauris quis arcu quis tortor imperdiet viverra. Sed ut iaculis",
-                link            : 'https://www.youtube.com/watch?v=USQdTLjYM0E'
+                link            : 'https://www.youtube.com/watch?v=COzxCjRZpys'
             }, 
             {
                 downstreamTitle : "Manufacturing Execution Systems",
                 downstreamimg   : "/images/Illustration_3.png",
                 downstreamtext  : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pellentesque, elit id pharetra cursus, turpis ex mollis magna, eu fringilla urna ante ut tellus. Cras imperdiet tristique venenatis Vivamus elementum enim ipsum, nec pharetra sapien ornare eu. Mauris quis arcu quis tortor imperdiet viverra. Sed ut iaculis",
-                 link           : 'https://www.youtube.com/watch?v=Ra7HPPaql5s', 
+                 link           : 'https://www.youtube.com/watch?v=USQdTLjYM0E&t=3s', 
             },
             {
                 downstreamTitle : "Manufacturing Execution Systems",
                 downstreamimg   : "/images/Illustration_3.png",
                 downstreamtext  : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pellentesque, elit id pharetra cursus, turpis ex mollis magna, eu fringilla urna ante ut tellus. Cras imperdiet tristique venenatis Vivamus elementum enim ipsum, nec pharetra sapien ornare eu. Mauris quis arcu quis tortor imperdiet viverra. Sed ut iaculis",
-                 link           : 'https://www.youtube.com/watch?v=olFO-Y_cGwU', 
+                 link           : 'https://www.youtube.com/watch?v=Ra7HPPaql5s', 
             }
         ]
     }
@@ -43,13 +43,13 @@ export default class Resourcevideo extends Component {
                 downstreamimg   : "/images/Illustration_2.png",
                 video1st        : "https://iogsolutions-my.sharepoint.com/personal/anurag_shinde_iogsolutions_com/_layouts/15/onedrive.aspx?originalPath=aHR0cHM6Ly9pb2dzb2x1dGlvbnMtbXkuc2hhcmVwb2ludC5jb20vOmY6L3AvYW51cmFnX3NoaW5kZS9Fc0VrNEo3cWtVaEx1UlRNNjBEUUttTUJndDB1WElqTjUwRmNjLU5ZT3dIUEp3P3J0aW1lPWFfRjg1Wlp0MTBn&id=%2Fpersonal%2Fanurag%5Fshinde%5Fiogsolutions%5Fcom%2FDocuments%2FMarketing%20Activities%2FWebsite%20Revamp%2FVideos%2F05%20iOG%20SCM%2Emp4&parent=%2Fpersonal%2Fanurag%5Fshinde%5Fiogsolutions%5Fcom%2FDocuments%2FMarketing%20Activities%2FWebsite%20Revamp%2FVideos",
                 downstreamtext  : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pellentesque, elit id pharetra cursus, turpis ex mollis magna, eu fringilla urna ante ut tellus. Cras imperdiet tristique venenatis Vivamus elementum enim ipsum, nec pharetra sapien ornare eu. Mauris quis arcu quis tortor imperdiet viverra. Sed ut iaculis",
-                link            : 'https://iogsolutions-my.sharepoint.com/:v:/r/personal/anurag_shinde_iogsolutions_com/Documents/Marketing%20Activities/Website%20Revamp/Videos/02%20iOG%20Downstream.mp4?csf=1&e=bVUHuU',
+                link            : 'https://www.youtube.com/watch?v=olFO-Y_cGwU&t=8s',
             }, 
             {
                 downstreamTitle : "Manufacturing Execution Systems",
                 downstreamimg   : "/images/Illustration_3.png",
                 downstreamtext  : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pellentesque, elit id pharetra cursus, turpis ex mollis magna, eu fringilla urna ante ut tellus. Cras imperdiet tristique venenatis Vivamus elementum enim ipsum, nec pharetra sapien ornare eu. Mauris quis arcu quis tortor imperdiet viverra. Sed ut iaculis",
-                 link           : 'https://iogsolutions-my.sharepoint.com/:v:/r/personal/anurag_shinde_iogsolutions_com/Documents/Marketing%20Activities/Website%20Revamp/Videos/01%20iOG%20Consult.mp4?csf=1&e=11faBb', 
+                 link           : 'https://www.youtube.com/channel/UCDMmcwbVtH32u6RC31CseDw?app=desktop', 
             },
            
         ]
